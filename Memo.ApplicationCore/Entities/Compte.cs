b53794050df0c7a24c68a0643e3fe51c9e2a2238
abela <abela@ALEX>
2025-09-04 17:37:55 -0400

    Added entites + IAsyncRepository to Application Core

---
+++ Memo.ApplicationCore/Entities/Compte.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memo.ApplicationCore.Entities
+{
+        public class Compte
+        {
+            [Key]
+            public string Id { get; set; }
+            [Required]
+            [StringLength(150)]
+            public string NomUtilisateur { get; set; }
+            [Required]
+            [StringLength(150)]
+            public string MotDePasse { get; set; }
+            [Required]
+            public DateTime DateCreation { get; set; }
+            public DateTime? DateDerniereConnexion { get; set; }
+
+            public ICollection<Memo> Memos { get; set; } = new List<Memo>();
+        }
+}
---
+++ Memo.ApplicationCore/Entities/Memo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memo.ApplicationCore.Entities
+{
+    public class Memo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Titre { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Description { get; set; }
+        [Required]
+        public DateTime DateCreation { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string IdCompte { get; set; }
+
+        public Compte Compte { get; set; }
+    }
+}
