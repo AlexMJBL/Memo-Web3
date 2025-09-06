96a0ed4b1c4ad146c60576cdd6b306a55bcf9058
abela <abela@ALEX>
2025-09-06 13:51:50 -0400

    Added login component ts + html.

--- MemoWeb/src/app/login/login.component.ts
+++ MemoWeb/src/app/login/login.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { Compte } from '../models/compte';
+import { CompteService } from '../services/compte.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  infoConnexion: Compte = {motDePasse : '', nomUtilisateur : ''};
+
+  constructor(private compteService: CompteService, private routeur: Router, private toastr: ToastrService){ }
+
+  seConnecter(){
+
+    if (!this.infoConnexion.nomUtilisateur || !this.infoConnexion.motDePasse) {
+    this.toastr.error('Veuillez remplir tous les champs');
+    return;
+    }
+
+    this.compteService.seConnecter(this.infoConnexion).subscribe(
+    {
+      next: () => {
+        this.routeur.navigateByUrl('/home');
+      },
+      error: erreur => this.toastr.error(erreur.error)
+    });
+  }
 }
